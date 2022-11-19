17ea5e19cf8a618e9171b9bf3c26c82ed67bdc5b
Ahmad Essam <61994278+AHMAD19858@users.noreply.github.com>
2022-11-19 22:44:50 +0200

    initial commit

---
+++ src/Data/Data.js
@@ -0,0 +1,117 @@
+// Sidebar imports
+import {
+  UilEstate,
+  UilClipboardAlt,
+  UilUsersAlt,
+  UilPackage,
+  UilChart,
+  UilSignOutAlt,
+} from "@iconscout/react-unicons";
+
+// Analytics Cards imports
+import { UilUsdSquare, UilMoneyWithdrawal } from "@iconscout/react-unicons";
+import { keyboard } from "@testing-library/user-event/dist/keyboard";
+
+// Recent Card Imports
+import img1 from "../imgs/img1.png";
+import img2 from "../imgs/img2.png";
+import img3 from "../imgs/img3.png";
+
+// Sidebar Data
+export const SidebarData = [
+  {
+    icon: UilEstate,
+    heading: "Dashboard",
+  },
+  {
+    icon: UilClipboardAlt,
+    heading: "Transaction",
+  },
+  {
+    icon: UilUsersAlt,
+    heading: "Task",
+  },
+  {
+    icon: UilPackage,
+    heading: 'Documents'
+  },
+  {
+    icon: UilChart,
+    heading: 'Store'
+  },
+];
+
+// Analytics Cards Data
+export const cardsData = [
+  {
+    title: "Documents",
+    color: {
+      backGround: "linear-gradient(180deg, #bb67ff 0%, #c484f3 100%)",
+      boxShadow: "0px 10px 20px 0px #e0c6f5",
+    },
+    barValue: 70,
+    value: "25,970",
+    png: UilUsdSquare,
+    giga: "2.1 GB",
+    series: [
+      {
+        name: "Google Drive",
+        data: [31, 40, 28, 51, 42, 109, 100],
+      },
+    ],
+  },
+  {
+    title: "Google Drive",
+    color: {
+      backGround: "linear-gradient(180deg, #FF919D 0%, #FC929D 100%)",
+      boxShadow: "0px 10px 20px 0px #FDC0C7",
+    },
+    barValue: 80,
+    value: "14,270",
+    giga: "5.1 GB",
+    png: UilMoneyWithdrawal,
+    series: [
+      {
+        name: "Revenue",
+        data: [10, 100, 50, 70, 80, 30, 40],
+      },
+    ],
+  },
+  {
+    title: "Dropbox",
+    color: {
+      backGround:
+        "linear-gradient(rgb(248, 212, 154) -146.42%, rgb(255 202 113) -46.42%)",
+      boxShadow: "0px 10px 20px 0px #F9D59B",
+    },
+    barValue: 60,
+    giga: "7.1 GB",
+    value: "4,270",
+    png: UilClipboardAlt,
+    series: [
+      {
+        name: "Expenses",
+        data: [10, 25, 15, 30, 12, 15, 20],
+      },
+    ],
+  },
+];
+
+// Recent Update Card Data
+export const UpdatesData = [
+  {
+    img: img1,
+    name: "Ahmed",
+    time: "25 seconds ago",
+  },
+  {
+    img: img2,
+    name: "Omar",
+    time: "30 minutes ago",
+  },
+  {
+    img: img3,
+    name: "Natasha",
+    time: "2 hours ago",
+  },
+];
